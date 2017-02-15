4356768fd089c22c598d9b729b4fd5ba0b0be659
HulkHou <houxiang86@gmail.com>
2017-02-15 14:47:50 +0800

    update .gitignore

--- jlight-web/target/classes/static/js/system/controller/LoginLogCtrl.js
+++
@@ -1,4 +0,0 @@
-var loginLogApp = angular.module('loginLogApp', ['base']);
-loginLogApp.controller('loginLogCtrl', ['$rootScope', '$scope',function ($rootScope,$scope) {
-    $scope.queryFilter = {};
- }]);
--- jlight-web/target/classes/static/js/system/controller/WebLogCtrl.js
+++
@@ -1,4 +0,0 @@
-var webLogApp = angular.module('webLogApp', ['base']);
-webLogApp.controller('webLogCtrl', ['$rootScope', '$scope',function ($rootScope,$scope) {
-	$scope.queryFilter = {};
- }]);
